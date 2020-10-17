b7291ab8cf5ced1b284ea120cb7f39aa05241e39
trainningjava <48299029+trainningjava@users.noreply.github.com>
2020-10-17 15:22:05 -0300

    init

---
+++ web/src/routes.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Landing from "./pages/Landing";
+import OrphanagesMap from "./pages/OrphanagesMap";
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
+
+
+
+function Routes () {
+  return (
+    <Router>
+      <Switch>
+      <Route path="/" exact component={Landing} />
+      <Route path="/app" component={OrphanagesMap} />
+      
+    <Route path="/orphanages/create" component ={CreateOrphanage}/>
+    <Route path="/orphanages/:id" component ={Orphanage}/>      
+      </Switch>
+    </Router>
+  );
+}
+
+export default Routes;
